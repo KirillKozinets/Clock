b6eeb1daed8f37fdfc4e8a28b7ef42ec2470ce79
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-08-29 17:56:56 +0700

    Изменена версия

--- app/src/main/java/com/sgc/clock/db/AlarmClockDatabase.java
+++ app/src/main/java/com/sgc/clock/db/AlarmClockDatabase.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.RoomDatabase;
 
 import com.sgc.clock.model.AlarmClock;
 
-@Database(entities = {AlarmClock.class}, version = 8)
+@Database(entities = {AlarmClock.class}, version = 9)
 public abstract class AlarmClockDatabase extends RoomDatabase {
     public abstract AlarmClockDao employeeDao();
 }
