7e5866ffee03d452de42e39f5b7bd18ebcf2df81
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-08-29 17:56:44 +0700

    Переделан в singlton

--- app/src/main/java/com/sgc/clock/db/AlarmClockDataBaseHelper.java
+++ app/src/main/java/com/sgc/clock/db/AlarmClockDataBaseHelper.java
@@ -7,10 +7,21 @@ import com.sgc.clock.model.AlarmClock;
 
 public class AlarmClockDataBaseHelper {
 
+    private static AlarmClockDataBaseHelper instance;
+
+    public static AlarmClockDataBaseHelper getInstance(Context context)
+    {
+        if (instance== null) {
+                    instance = new AlarmClockDataBaseHelper(context);
+        }
+
+        return instance;
+    }
+
     private static AlarmClockDatabase dataBase;
     private static AlarmClockDao databaseHandler;
 
-    public static void loadDataBase(Context context) {
+    private AlarmClockDataBaseHelper(Context context) {
         if (dataBase == null || databaseHandler == null) {
             dataBase = Room.databaseBuilder(context,
                     AlarmClockDatabase.class, "alarmClock")
@@ -21,22 +32,15 @@ public class AlarmClockDataBaseHelper {
         }
     }
 
-    public static void addAlarmClockToDataBase(AlarmClock alarmClock) {
-        isDataBaseLoad();
+    public void addAlarmClockToDataBase(AlarmClock alarmClock) {
         databaseHandler.addAlarmClock(alarmClock);
     }
 
-    public static void гзвфеуAlarmClockToDataBase(AlarmClock alarmClock) {
-        isDataBaseLoad();
+    public void insertAlarmClockToDataBase(AlarmClock alarmClock) {
         int updateAlarmClockCount = databaseHandler.updateAlarmClock(alarmClock);
 
         if(updateAlarmClockCount == 0)
             addAlarmClockToDataBase(alarmClock);
-    }
-
-    private static void isDataBaseLoad() {
-        if (dataBase == null || databaseHandler == null)
-            throw new RuntimeException("Database was not loaded. Check if the loadDataBase() method was called");
     }
 
 
