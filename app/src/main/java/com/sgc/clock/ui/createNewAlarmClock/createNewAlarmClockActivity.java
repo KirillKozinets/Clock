e60fe337fa7033d6eae0426076f76fd84eba76dd
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-09-01 16:41:06 +0700

    Добавлены импорты тегов

--- app/src/main/java/com/sgc/clock/ui/createNewAlarmClock/createNewAlarmClockActivity.java
+++ app/src/main/java/com/sgc/clock/ui/createNewAlarmClock/createNewAlarmClockActivity.java
@@ -1,6 +1,7 @@
 package com.sgc.clock.ui.createNewAlarmClock;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -20,9 +21,14 @@ import com.sgc.clock.R;
 import com.sgc.clock.db.AlarmClockDataBaseHelper;
 import com.sgc.clock.model.AlarmClock;
 
+import java.util.Arrays;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static com.sgc.clock.ui.alarmClock.alarmClockFragment.TAG_ACTIVITY_CREATE_ALARM_CLOCK_TITLE;
+import static com.sgc.clock.ui.alarmClock.alarmClockFragment.TAG_SEND_ID_TO_CHANGE_ALARM_CLOCK;
 
 public class createNewAlarmClockActivity extends AppCompatActivity {
 
@@ -38,6 +44,8 @@ public class createNewAlarmClockActivity extends AppCompatActivity {
     ConstraintLayout alarmClockNameChange;
     @BindView(R.id.description)
     TextView alarmClockName;
+    @BindView(R.id.title)
+    TextView mainTitle;
 
     private TextView hoursLastTextView;
     private TextView minutesLastTextView;
@@ -48,10 +56,13 @@ public class createNewAlarmClockActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager hoursLayoutManager = new LinearLayoutManager(this);
     private RecyclerView.LayoutManager minutesLayoutManager = new LinearLayoutManager(this);
 
-    private int hoursPosition = 2408;
+    private int startAlarmClockOffset = 8;
+    private int hoursPosition = 2400 + startAlarmClockOffset;
     private int minutesPosition = 2400;
 
-    String description = "Будильник";
+    private String alarmClockDescription = "Будильник";
+    private boolean isChangeAlarmClock = false;
+    private int alarmClockChangeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +75,7 @@ public class createNewAlarmClockActivity extends AppCompatActivity {
         TimeSelectAdapter adapterMinutes = new TimeSelectAdapter(this, 60, 1, 2);
 
         loadInstanceState(savedInstanceState);
+        loadAlarmClockFromIntent();
 
         installationRecyclerView(
                 hours, hoursSnapHelper, hoursLayoutManager,
@@ -74,6 +86,50 @@ public class createNewAlarmClockActivity extends AppCompatActivity {
                 adapterMinutes, recyclerViewScrollListenerMinutes, minutesPosition);
     }
 
+    private void loadAlarmClockFromIntent() {
+        Intent intent = getIntent();
+        int alarmClockId = intent.getIntExtra(TAG_SEND_ID_TO_CHANGE_ALARM_CLOCK, -1);
+        String title = intent.getStringExtra(TAG_ACTIVITY_CREATE_ALARM_CLOCK_TITLE);
+
+        if (alarmClockId != -1) {
+            showAlarmClockFromIntent(alarmClockId);
+            alarmClockChangeId = alarmClockId;
+            isChangeAlarmClock = true;
+        }
+
+        if (title != null)
+            mainTitle.setText(title);
+
+    }
+
+    private void showAlarmClockFromIntent(int id) {
+        AlarmClockDataBaseHelper alarmClockDataBaseHelper = AlarmClockDataBaseHelper.getInstance(getApplicationContext());
+        AlarmClock alarmClock = alarmClockDataBaseHelper.getAlarmClock(id);
+
+        showAlarmClockTimeFromIntent(alarmClock);
+        showAlarmClockDescription(alarmClock);
+    }
+
+    private void showAlarmClockTimeFromIntent(AlarmClock alarmClock) {
+        int alarmClockTimeHours;
+        int alarmClockTimeMinutes;
+
+        String[] alarmClockTime = alarmClock.getAlarmClockTime().split(" : ");
+        if (alarmClockTime.length == 2) {
+            alarmClockTimeHours = Integer.parseInt(alarmClockTime[0]);
+            alarmClockTimeMinutes = Integer.parseInt(alarmClockTime[1]);
+        } else
+            throw new RuntimeException("alarm clock time wrong format. Incomprehensible format : " + Arrays.toString(alarmClockTime));
+
+        hoursPosition = hoursPosition - startAlarmClockOffset + alarmClockTimeHours;
+        minutesPosition = minutesPosition + alarmClockTimeMinutes;
+    }
+
+    private void showAlarmClockDescription(AlarmClock alarmClock) {
+        String alarmClockName = alarmClock.getAlarmClockName();
+        this.alarmClockName.setText(alarmClockName);
+        alarmClockDescription = alarmClockName;
+    }
 
     private void installationRecyclerView(RecyclerView recyclerView, LinearSnapHelper snapHelper,
                                           RecyclerView.LayoutManager layoutManager, TimeSelectAdapter adapter,
@@ -91,8 +147,8 @@ public class createNewAlarmClockActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             hoursPosition = savedInstanceState.getInt("hours");
             minutesPosition = savedInstanceState.getInt("minutes");
-            description = savedInstanceState.getString("description");
-            alarmClockName.setText(description);
+            alarmClockDescription = savedInstanceState.getString("description");
+            alarmClockName.setText(alarmClockDescription);
         }
     }
 
@@ -147,11 +203,17 @@ public class createNewAlarmClockActivity extends AppCompatActivity {
     @SuppressLint("DefaultLocale")
     @OnClick(R.id.addAlarmClock)
     public void addNewAlarmClock() {
+        AlarmClockDataBaseHelper alarmClockDataBaseHelper = AlarmClockDataBaseHelper.getInstance(this);
+
         String hours = hoursLastTextView.getText().toString();
         String minutes = String.format("%02d", Integer.parseInt(minutesLastTextView.getText().toString()));
         String alarmTime = hours + " : " + minutes;
 
-        AlarmClockDataBaseHelper.getInstance(this).addAlarmClockToDataBase(new AlarmClock(description, alarmTime, "Пн - Пт", true));
+        if (!isChangeAlarmClock)
+            alarmClockDataBaseHelper.addAlarmClockToDataBase(new AlarmClock(alarmClockDescription, alarmTime, "Пн - Пт", true));
+        else
+            alarmClockDataBaseHelper.updateAlarmClockToDataBase(new AlarmClock(alarmClockChangeId, alarmClockDescription, alarmTime, "Пн - Пт", true));
+
         onBackPressed();
     }
 
@@ -174,12 +236,13 @@ public class createNewAlarmClockActivity extends AppCompatActivity {
 
         ok.setOnClickListener(view -> changeAlarmClockDescription(description, alertDialog));
         cancel.setOnClickListener(view -> alertDialog.cancel());
+        description.setText(alarmClockDescription);
     }
 
     private void changeAlarmClockDescription(EditText description, AlertDialog alertDialog) {
-        this.description = description.getText().toString();
+        alarmClockDescription = description.getText().toString();
         alertDialog.cancel();
-        alarmClockName.setText(this.description);
+        alarmClockName.setText(alarmClockDescription);
     }
 
     private AlertDialog createChangeDescriptionAlertDialog(View promptsView) {
