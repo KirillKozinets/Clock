65245480c6c38b396c33495b3607e33e0dbd5e9b
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-09-08 16:05:34 +0700

    Добавлен тест

---
+++ app/src/androidTest/java/com/sgc/clock/util/AlarmClockConverterTest.java
@@ -0,0 +1,26 @@
+package com.sgc.clock.util;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+
+import com.sgc.clock.model.AlarmClock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class AlarmClockConverterTest extends AndroidTestCase {
+
+    @Test
+    public void testConvertByteArrayToAlarmClock() {
+        AlarmClock alarmClockTest = new AlarmClock("feefef", "gdfdfdf", "gffgfgfg", true);
+        byte[] alarmClockTestByteArray = ParcelableUtil.marshall(alarmClockTest);
+        AlarmClock alarmClockTest2 = AlarmClockConverter.convertByteArrayToAlarmClock(alarmClockTestByteArray);
+        assertTrue(alarmClockTest.equals(alarmClockTest2));
+    }
+}
