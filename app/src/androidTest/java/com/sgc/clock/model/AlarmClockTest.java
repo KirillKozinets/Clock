cfe7375bed1db5d4fb8f5cb9f7587a5bde222009
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-09-08 16:05:25 +0700

    Добавлен тест

---
+++ app/src/androidTest/java/com/sgc/clock/model/AlarmClockTest.java
@@ -0,0 +1,91 @@
+package com.sgc.clock.model;
+
+import android.content.Intent;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+
+import com.sgc.clock.model.AlarmClock;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class AlarmClockTest extends AndroidTestCase {
+    AlarmClock alarmClock;
+
+    String testTime = "15 : 45";
+    String testName = "testName";
+    String dayOfWeek = "testBane";
+    boolean active = false;
+
+    @Before
+    public void setUp() {
+        alarmClock = new AlarmClock(testName, testTime, dayOfWeek, active);
+    }
+
+    @Test
+    public void testParseTime() {
+        int testHorse;
+        int testMinutes;
+        int trueHorse = 15;
+        int trueMinutes = 45;
+
+        testHorse = alarmClock.getHorse();
+        testMinutes = alarmClock.getMinutes();
+
+        assertEquals(testHorse, trueHorse);
+        assertEquals(testMinutes, trueMinutes);
+    }
+
+    @Test
+    public void testCorrectFillField() {
+        assertEquals(testTime, alarmClock.getAlarmClockTime());
+        assertEquals(testName, alarmClock.getAlarmClockName());
+        assertEquals(dayOfWeek, alarmClock.getAlarmClockDaysOfWeek());
+        assertEquals(active, alarmClock.getActive());
+    }
+
+    @Test
+    public void testTrueEquals() {
+        AlarmClock alarmClock1 = new AlarmClock(alarmClock);
+        AlarmClock alarmClock2 = new AlarmClock(alarmClock);
+        assertTrue(alarmClock1.equals(alarmClock2));
+    }
+
+    @Test
+    public void testFalseEquals1() {
+        AlarmClock alarmClock1 = new AlarmClock(alarmClock);
+        AlarmClock alarmClock2 = new AlarmClock(alarmClock);
+
+        alarmClock2.setAlarmClockTime("44444");
+
+        assertFalse(alarmClock1.equals(alarmClock2));
+    }
+
+    @Test
+    public void testFalseEquals2() {
+        AlarmClock alarmClock1 = new AlarmClock(alarmClock);
+        AlarmClock alarmClock2 = new AlarmClock(alarmClock);
+
+        alarmClock2.setAlarmClockName("44444");
+
+        assertFalse(alarmClock1.equals(alarmClock2));
+    }
+
+    @Test
+    public void testFalseEquals3() {
+        AlarmClock alarmClock1 = new AlarmClock(alarmClock);
+        AlarmClock alarmClock2 = new AlarmClock(alarmClock);
+
+        alarmClock2.setActive(true);
+
+        assertFalse(alarmClock1.equals(alarmClock2));
+    }
+
+}
