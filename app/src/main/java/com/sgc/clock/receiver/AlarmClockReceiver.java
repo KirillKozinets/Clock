8ec36cd301d0f45b4dd3e206257c39c57c2a8649
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-09-09 16:27:40 +0700

    Рефакторинг

--- app/src/main/java/com/sgc/clock/receiver/AlarmClockReceiver.java
+++ app/src/main/java/com/sgc/clock/receiver/AlarmClockReceiver.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.AppLaunchChecker;
 
+import com.sgc.clock.model.AlarmClock;
 import com.sgc.clock.service.OrderReminderNotificationService;
 import com.sgc.clock.util.AlarmClockConverter;
 import com.sgc.clock.util.AlarmManagerUtil;
@@ -19,15 +20,36 @@ public class AlarmClockReceiver extends BroadcastReceiver {
         byte[] alarmClockByteArray = intent.getByteArrayExtra("alarmClock");
         i.putExtra("alarmClock", alarmClockByteArray);
 
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        if (checkReboot(intent))
             AlarmManagerUtil.setAlarm(AlarmClockConverter.convertByteArrayToAlarmClock(alarmClockByteArray), context);
-        }
-        if (Intent.ACTION_TIME_CHANGED.equals(i.getAction()) || Intent.ACTION_TIMEZONE_CHANGED.equals(i.getAction())) {
+
+        if (checkTimeChanged(intent)) {
             AlarmManagerUtil.cancel(AlarmClockConverter.convertByteArrayToAlarmClock(alarmClockByteArray), context);
             AlarmManagerUtil.setAlarm(AlarmClockConverter.convertByteArrayToAlarmClock(alarmClockByteArray), context);
         }
 
         context.startService(i);
+
         setResultCode(Activity.RESULT_OK);
+
+    }
+
+    private boolean checkReboot(Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkTimeChanged(Intent intent) {
+        String intentAction = intent.getAction();
+
+        if (Intent.ACTION_TIME_CHANGED.equals(intentAction)
+                || Intent.ACTION_TIMEZONE_CHANGED.equals(intentAction)) {
+
+            return true;
+        }
+
+        return false;
     }
 }
